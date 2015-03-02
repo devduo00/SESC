cd12673f62f16c5d52a45bba85a7aeb7ad2f3b63
makatijet <makatijet@gmail.com>
2015-03-02 14:37:36 +0800

    facebook 1st app sample

--- index.php
+++ index.php
@@ -1,7 +1,26 @@
 <?php
-    //$session    = FacebookSession::setDefaultApplication('1381794498804715', 'dbcf7985ae7d57274665c75dcbe5b1d0');
-
+    use Facebook\FacebookSession;
     use Facebook\GraphUser;
+
+    FacebookSession::setDefaultApplication('1381794498804715', 'dbcf7985ae7d57274665c75dcbe5b1d0');
+
+    // If you already have a valid access token:
+    $session = new FacebookSession('00e77536cc63860b0624bd1383dfe21d');
+
+    // If you're making app-level requests:
+    $session = FacebookSession::newAppSession();
+
+    // To validate the session:
+    try {
+        $session->validate();
+    } catch (FacebookRequestException $ex) {
+        // Session not valid, Graph API returned an exception with the reason.
+        echo $ex->getMessage();
+    } catch (\Exception $ex) {
+        // Graph API returned info, but it may mismatch the current app or have expired.
+        echo $ex->getMessage();
+    }
+
 
     // Get the base class GraphObject from the response
     $object = $response->getGraphObject();
