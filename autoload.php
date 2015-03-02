2686f72987911e90888a2fb49cc8e5b2ab298d6e
makatijet <makatijet@gmail.com>
2015-03-02 22:20:11 +0800

    facebook SDK namespace

--- autoload.php
+++ autoload.php
@@ -7,4 +7,13 @@
     require_once( 'Facebook/FacebookRequestException.php' );
     require_once( 'Facebook/FacebookAuthorizationException.php' );
     require_once( 'Facebook/GraphObject.php' );
+    require_once( 'Facebook/GraphObject.php');
+    require_once( 'Facebook/GraphUser.php');
+    require_once( 'Facebook/GraphSessionInfo.php');
+    require_once( 'Facebook/GraphSessionInfo.php');
+    require_once( 'Facebook/Entities/AccessToken.php');
+    require_once( 'Facebook/HttpClients/FacebookCurl.php');
+    require_once( 'Facebook/HttpClients/FacebookHttpable.php');
+    require_once( 'Facebook/HttpClients/FacebookCurlHttpClient.php');
+
 ?>
