f997b73c82e2584c4339d29c8b04e5eb12294d3a
makatijet <makatijet@gmail.com>
2015-03-02 21:29:44 +0800

    facebook SDK namespace

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-/*
+
     session_start();
     require_once 'autoload.php';
 
@@ -9,56 +9,10 @@
     use Facebook\FacebookRedirectLoginHelper;
     use Facebook\FacebookRequestException;
 
-    echo "1";
-
     FacebookSession::setDefaultApplication('1381794498804715', 'dbcf7985ae7d57274665c75dcbe5b1d0');
-/*
-    echo "2";
-    // If you already have a valid access token:
-    //$session = new FacebookSession('00e77536cc63860b0624bd1383dfe21d');
 
-    echo "3";
-
-    // If you're making app-level requests:
-    $session = FacebookSession::newAppSession('1381794498804715', 'dbcf7985ae7d57274665c75dcbe5b1d0');
-
-    echo "4";
-    echo "session";
-    echo $session;
-
-    // To validate the session:
-    try {
-        $session->validate();
-    } catch (FacebookRequestException $ex) {
-        // Session not valid, Graph API returned an exception with the reason.
-        echo $ex->getMessage();
-    } catch (\Exception $ex) {
-        // Graph API returned info, but it may mismatch the current app or have expired.
-        echo $ex->getMessage();
-    }
-
-
-    try {
-        $response = (new FacebookRequest($session, 'GET', '/me'))->execute();
-        $object = $response->getGraphObject();
-        echo $object->getProperty('name');
-    } catch (FacebookRequestException $ex) {
-        echo $ex->getMessage();
-    } catch (\Exception $ex) {
-        echo $ex->getMessage();
-    }
-
-    // You can chain methods together and get a strongly typed GraphUser
-    $me = (new FacebookRequest(
-        $session, 'GET', '/me'
-    ))->execute()->getGraphObject(GraphUser::className);
-    echo $me->getName();
-*/
-/*
     // login helper with redirect_uri
     $helper = new FacebookRedirectLoginHelper( 'http://180.70.94.239:8080/fb/SESC/' );
-
-    echo "2";
 
     try {
         $session = $helper->getSessionFromRedirect();
@@ -68,9 +22,6 @@
         // When validation fails or other local issues
     }
 
-    echo "3";
-
-    // see if we have a session
     if ( isset( $session ) ) {
         // graph api request for user data
         $request = new FacebookRequest( $session, 'GET', '/me' );
@@ -84,7 +35,7 @@
         // show login url
         echo '<a href="' . $helper->getLoginUrl() . '">Login</a>';
     }
-*/
+
 ?>
 
 <!doctype html>
